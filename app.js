be010b4a205865b9de68548bcf1fbfd40a8a00af
lengoc2311 <lngc2311@gmail.com>
2022-03-12 19:59:05 +0700

    update

--- app.js
+++ app.js
@@ -78,14 +78,19 @@ app.post('/insert',async (req,res)=>{
     const name = req.body.txtName
     const price = req.body.txtPrice
     const picURL = req.body.txtPictureURL
-    const newP = {
-        'name' : name,
-        'price': price,
-        'pic' : picURL
-    }
+    if(name.length <5){
+        res.render('index', {nameError:"name must more than 5 characters"})
+        return
+    }else{
+        const newP = {
+            'name' : name,
+            'price': price,
+            'pic' : picURL
+        }
     const collectionName = 'SanPham'
     await insertObject(collectionName,newP)
     res.render('index')
+    } 
 })
 
 const PORT = process.env.PORT || 5000
